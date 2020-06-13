c704a483f4b5e784064e04a9b97d76951e0f9b38
88KirilN <kiril.naumoski@students.finki.ukim.mk>
2020-06-13 18:04:21 +0200

    commented code

---
+++ src/main/java/SILab2.java
@@ -0,0 +1,55 @@
+import java.util.List;
+
+class User {
+    String username;
+    String password;
+    String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+public class SILab2 {
+
+    public boolean function (User user, List<String> allUsers) { //A
+        if (user==null) { //B
+            throw new RuntimeException("The user argument is not initialized!"); //C
+        }
+        else { //D
+            if (user.getUsername()==null || allUsers.contains(user.getUsername())) { //E
+                throw new RuntimeException("User already exists!"); //F
+            }
+            else { //G
+                if (user.getEmail()==null) //H
+                    return false; //I
+                boolean atChar = false, dotChar = false; //J
+                for (int i=0;i<user.getEmail().length();i++) { //K
+                    if (user.getEmail().charAt(i)=='@') //L
+                        atChar = true; //M
+                    if (atChar && user.getEmail().charAt(i)=='.') { //N
+                        dotChar = true; //O
+                    }
+                } //P
+                if (!atChar || !dotChar) { //Q
+                    return false; //R
+                }
+            }
+        }
+        return true; //S
+    } //T
+}
